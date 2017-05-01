4a71b536b25f3c079aa1136d9cbf9c36325b1249
pearsegorman18 <pearsegorman18@gmail.com>
2017-05-01 01:46:10 +0100

    Image magnify added, tidy up of display

--- js/script-p2.js
+++ js/script-p2.js
@@ -1,13 +1,15 @@
 // JavaScript Document
 
+"use strict";
 
+// jflow image slider when page loads
 $(document).ready(function() {
     $("#controller").jFlow({
         slides: "#slides",
 
-        width: "1400px",
+        width: "670px",
 
-        height: "513px",
+        height: "420px",
 
         duration: 2000,  // time in milliseconds to transition one slide
 
@@ -16,4 +18,41 @@ $(document).ready(function() {
         effect: "rewind", //this is the slide effect (rewind or flow)
 
     });
+	
+	
+	// applies image magnify to images of the class magSlides
+	jQuery(".magSlides").imageMagnify({ 
+ 		
+		magnifyby:1.5,
+		
+		magnifyduration: 1000,
+		
+		mgopacity: 0.2 //opacify of original image when image is enlarged 
+	});
+	
 });
+
+//Image Power Zoomer v1.2
+// http://dynamicdrive.com/dynamicindex4/powerzoomer.htm
+
+// Code I tried to use for another magnifying effect.
+// when the image in the slider was magnified by the first plugin,
+// I wanted this plugin to magnify the image once again.
+// this plugin is used as a magnifying glass on the areas of the image
+// you place the cursor over
+
+//$(".magSlides").click(function(){ 
+//$(this).addpowerzoom({
+// 		defaultpower: 2,
+//		
+// 		powerrange: [2,5],
+//		
+// 		largeimage: null,
+//		
+// 		magnifiersize: [100,100] //<--no comma following last option!
+//	});
+//
+//});
+//});
+
+
